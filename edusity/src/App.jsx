309bb3f0d9fe748519947cc41174d1426c564827
sujan <budhasujan3@gmail.com>
2024-08-04 20:20:22 +0545

    Programmes and title part of web site

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -1,11 +1,17 @@
 import Hero from "./components/hero/hero"
 import Navbar from "./components/navbar/Navbar"
+import Programs from "./components/programs/Programs"
+import Title from "./components/title/Title"
 
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/>
+      <div className="container">
+        <Title subTitle='Our Program' title='What We Offer' />
+        <Programs/>
+      </div>
     </div>
   )
 }
