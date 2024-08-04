289eb3abbb8ff92f3c64429e913329a63c00e0d2
sujan <budhasujan3@gmail.com>
2024-08-04 19:43:43 +0545

    Hero section of oomponents

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -1,9 +1,11 @@
+import Hero from "./components/hero/hero"
 import Navbar from "./components/navbar/Navbar"
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <Hero/>
     </div>
   )
 }
---
+++ edusity/src/components/hero/hero.jsx
@@ -0,0 +1,16 @@
+import './hero.css';
+import dark_arrow from '../../assets/dark-arrow.png';
+
+const Hero = () => {
+  return (
+    <div className='hero container'>
+        <div className="hero-text">
+            <h1>We ensure better education for a better world</h1>
+            <p>Our cutting edge curriculu is designed to empower students with knowlede, skills and experiences needed to excel in the dynamic field of education</p>
+            <button className='btn'>Explore More <img src={dark_arrow} alt="" /></button>
+        </div>
+    </div>
+  )
+}
+
+export default Hero
