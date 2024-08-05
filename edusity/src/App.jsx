c6a28135674962bee4761c651d7ac1129c9fbf83
sujan <budhasujan3@gmail.com>
2024-08-05 21:35:47 +0545

    Slider component using react js

--- edusity/src/App.jsx
+++ edusity/src/App.jsx
@@ -3,6 +3,7 @@ import Campus from "./components/campus/Campus"
 import Hero from "./components/hero/hero"
 import Navbar from "./components/navbar/Navbar"
 import Programs from "./components/programs/Programs"
+import Testimonials from "./components/testimonials/Testimonials "
 import Title from "./components/title/Title"
 
 const App = () => {
@@ -17,6 +18,8 @@ const App = () => {
       <About/>
       <Title subTitle='Gallery' title='Campus Photos' />
       <Campus/>
+      <Title subTitle='Testimonials' title='What Students Says...' />
+      <Testimonials/>
     </div>
   )
 }
